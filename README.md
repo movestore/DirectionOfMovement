1317defc7783bbc27f40a5d0890b1c1e32476004
Anne Scharf <ascharf@ab.mpg.de>
2022-01-26 10:02:40 +0100

    changing name of app to avoid confusion

--- README.md
+++ README.md
@@ -1,20 +1,22 @@
-# Heading Of Segment Between Locations
+# Direction of movement
 
 MoveApps
 
 Github repository: https://github.com/movestore/Heading_BetweenLocations
 
 ## Description
-Calculation of the heading of each segment between consecutive locations.
+Calculation of the direction of movement of each segment between consecutive locations.
 
 ## Documentation
-This App calculates the heading of each segment between consecutive locations. This measurement is a segment characteristic, and will be assigned to the first location of each segment. Therefore the heading of the last location of the track will be set to NA. 
+This App calculates the direction of movement of each segment between consecutive locations. This measurement is a segment characteristic, and will be assigned to the first location of each segment. Therefore the direction of the last location of the track will be set to NA. 
 
-The headings are calculated as angles in degrees from -180 to 180. Where 0 corresponds to North, negative values go towards the West and positive values towards the East.
+The direction of movement is calculated as angles in degrees from -180 to 180. Where 0 corresponds to North, negative values go towards the West and positive values towards the East.
 
-A column named _**heading**_ will be appended to the dataset that is returned for further use in next Apps.
+A column named _**directionOfMovement**_ will be appended to the dataset that is returned for further use in next Apps.
 
-A histogram of the headings distribution of all individuals and per individual is automatically created and can be downloaded in the output as a pdf.
+A histogram of the directions distribution of all individuals and per individual is automatically created and can be downloaded in the output as a pdf.
+
+*Note*: the attribute **heading** is recorded by many tags and therefore present in many datasets. This value represents the instantaneous heading of the tag when the GPS location was recorded. This value will often not represent the direction of movement nor the position of the animal due to the placement of the tag (e.g. collar) or environmental conditions through which the animal is moving (e.g. bird is blown sideways by strong winds). 
 
 ### Input data
 moveStack in Movebank format
@@ -23,10 +25,10 @@ moveStack in Movebank format
 moveStack in Movebank format
 
 ### Artefacts
-`heading_histogram.pdf`: PDF with histograms of the headings per individual
+`directionOfMovement_histogram.pdf`: PDF with histograms of the azimuths per individual
 
 ### Parameters
 no parameters 
 
 ### Null or error handling
-**Data**: The full input dataset with the addition of headings is returned for further use in a next App and cannot be empty.
+**Data**: The full input dataset with the addition of the direction of movement is returned for further use in a next App and cannot be empty.
--- RFunction.R
+++ RFunction.R
@@ -2,22 +2,22 @@ library('move')
 library("ggplot2")
 
 rFunction <-  function(data) {
-  data$heading <- unlist(lapply(angle(data), function(x) c(as.vector(x), NA)))
+  data$directionOfMovement <- unlist(lapply(angle(data), function(x) c(as.vector(x), NA)))
     
     if(length(levels(trackId(data)))==1){
-      dataDF <- data.frame(heading=data$heading,indv=namesIndiv(data))
-      headingHist <- ggplot(dataDF, aes(heading))+geom_histogram(bins=100)+facet_grid(~indv)+ theme_bw()+ scale_x_continuous("Heading (deg)", breaks=c(-180,-90,0,90,180), labels=c("-180(S)","-90(W)","0(N)","90(E)","180(S)"), limits=c(-180,180))
-      pdf(paste0(Sys.getenv(x = "APP_ARTIFACTS_DIR", "/tmp/"), "heading_histogram.pdf"))
-      print(headingHist)
+      dataDF <- data.frame(directionOfMovement=data$directionOfMovement,indv=namesIndiv(data))
+      directionOfMovementHist <- ggplot(dataDF, aes(directionOfMovement))+geom_histogram(bins=100)+facet_grid(~indv)+ theme_bw()+ scale_x_continuous("Direction of movement (deg)", breaks=c(-180,-90,0,90,180), labels=c("-180(S)","-90(W)","0(N)","90(E)","180(S)"), limits=c(-180,180))
+      pdf(paste0(Sys.getenv(x = "APP_ARTIFACTS_DIR", "/tmp/"), "directionOfMovement_histogram.pdf"))
+      print(directionOfMovementHist)
       dev.off()
     } else {
-    pdf(paste0(Sys.getenv(x = "APP_ARTIFACTS_DIR", "/tmp/"), "heading_histogram.pdf"))
-      headingHistAll <- ggplot(data@data, aes(heading))+geom_histogram(bins=100)+ ggtitle("All Individuals") +theme_bw()+ scale_x_continuous("Heading (deg)", breaks=c(-180,-90,0,90,180), labels=c("-180(S)","-90(W)","0(N)","90(E)","180(S)"), limits=c(-180,180))
-    print(headingHistAll)
+    pdf(paste0(Sys.getenv(x = "APP_ARTIFACTS_DIR", "/tmp/"), "directionOfMovement_histogram.pdf"))
+      directionOfMovementHistAll <- ggplot(data@data, aes(directionOfMovement))+geom_histogram(bins=100)+ ggtitle("All Individuals") +theme_bw()+ scale_x_continuous("Direction of movement (deg)", breaks=c(-180,-90,0,90,180), labels=c("-180(S)","-90(W)","0(N)","90(E)","180(S)"), limits=c(-180,180))
+    print(directionOfMovementHistAll)
     lapply(split(data), function(x){
-      dataDF <- data.frame(heading=x$heading, indv=namesIndiv(x)) 
-      headingHist <- ggplot(dataDF, aes(heading))+geom_histogram(bins=100)+facet_grid(~indv)+ theme_bw()+ scale_x_continuous("Heading (deg)", breaks=c(-180,-90,0,90,180), labels=c("-180(S)","-90(W)","0(N)","90(E)","180(S)"), limits=c(-180,180))
-      print(headingHist)
+      dataDF <- data.frame(directionOfMovement=x$directionOfMovement, indv=namesIndiv(x)) 
+      directionOfMovementHist <- ggplot(dataDF, aes(directionOfMovement))+geom_histogram(bins=100)+facet_grid(~indv)+ theme_bw()+ scale_x_continuous("Direction of movement (deg)", breaks=c(-180,-90,0,90,180), labels=c("-180(S)","-90(W)","0(N)","90(E)","180(S)"), limits=c(-180,180))
+      print(directionOfMovementHist)
     })
     dev.off() 
     
